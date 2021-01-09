d500982d2acf72c9159d5dcb33065fcf335c0d2b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-09 15:18:50 -0500

    Created src/store/sagas/auth.js with the logOut generator

---
+++ src/store/sagas/auth.js
@@ -0,0 +1,20 @@
+// put is a function. It will dispatch a new action
+import {put} from 'redux-saga/effects';
+
+import * as actionTypes from "../actions/actionTypes";
+
+// The * symbol turns the function into a generator (Next gen JavaScript features)
+// Function that can be executed incrementally. Yo can call them and they dont't start from start to end immediately,
+// but you can pause during function execution (for example: to wait for asynchronous code to finish)
+
+function* logOut(action) {
+    // yield means that the step will be executed and then it will wait for it to finish
+    // In an asynchronous action it wouldn't continue before the step is done (the same here, on a synchronous action)
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('expirationDate');
+    yield localStorage.removeItem('localId');
+
+    put({
+        type: actionTypes.AUTH_LOG_OUT
+    });
+}
