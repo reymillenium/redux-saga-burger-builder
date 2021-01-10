606735795be0003e646db5804b652e9570585df9
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-10 18:31:56 -0500

    Modified src/store/sagas/orderFormSagas.js by moving the responses inside the try body

--- src/store/sagas/orderFormSagas.js
+++ src/store/sagas/orderFormSagas.js
@@ -5,9 +5,9 @@ import * as actionCreators from "./../actions/index";
 
 export function* purchaseBurgerSaga(action) {
     yield put(actionCreators.purchaseBurgerStart());
-    const response = yield axios.post('/orders.json?auth=' + action.payload.token, action.payload.order);
 
     try {
+        const response = yield axios.post('/orders.json?auth=' + action.payload.token, action.payload.order);
         yield put(actionCreators.purchaseBurgerSuccess(response.data.name, action.payload.order));
     } catch (error) {
         yield put(actionCreators.purchaseBurgerFail(error));
@@ -17,9 +17,9 @@ export function* purchaseBurgerSaga(action) {
 export function* fetchOrdersSaga(action) {
     yield put(actionCreators.fetchOrdersStart());
     const queryParams = yield `?auth=${action.payload.token}&orderBy="userId"&equalTo="${action.payload.userId}"`;
-    const response = yield axios.get('/orders.json' + queryParams)
 
     try {
+        const response = yield axios.get('/orders.json' + queryParams)
         const fetchedOrders = [];
         // Transforms the orders in array format
         for (let key in response.data) {
