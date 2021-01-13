f99ba3ea0f0d5a61ce4b56bc56f3746d9b4a442a
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-13 00:58:24 -0500

    Imported takeLatest and used in the watcher watchOrderForm for the listener linked to the purchaseBurgerSaga. Added some comments

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import {takeEvery, all} from 'redux-saga/effects';
+import {takeEvery, all, takeLatest} from 'redux-saga/effects';
 import * as actionTypes from './../actions/actionTypes';
 
 import {
@@ -24,6 +24,7 @@ export function* watchAuth(action) {
     // yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 
     // Another way of setting up the listeners, using the helper method 'all' this time:
+    // They are are executed simultaneously (multiple tasks at the same time)
     yield all([
         takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logOutSaga),
         takeEvery(actionTypes.AUTH_CHECK_TIME_OUT, checkAuthTimeOutSaga),
@@ -37,6 +38,12 @@ export function* watchBurgerBuilder(action) {
 }
 
 export function* watchOrderForm(action) {
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    // yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    // takeLatest will automatically cancel any ongoing execution of purchaseBurgerSaga and always...
+    // only executes the latest one (is useful from time to time to make sure that only of these...
+    // processes here is going on at a time)
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+
+    // With takeEvery it will execute fetchOrdersSaga whenever this action type (FETCH_ORDERS) is detected.
     yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
